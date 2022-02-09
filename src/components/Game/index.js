7abfdbba09fb614d80dc39b53e49010b314ceb71
Marydez <chukwumamary1@gmail.com>
2022-02-09 12:16:15 +0000

    nextplayer added

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,9 +2,11 @@ import './App.css';
 import { useState } from 'react';
 import Board from '../Board';
 function Game() {
+  const [nextPlayer, setNextPlayer] = useState("X")
   return (
     <div className="App">
       <Board />
+      <div> next player: {nextPlayer}</div>
     </div>
   );
 }
