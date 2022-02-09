b1ef823ebddc689114deeef314ff4ca5aa13ceca
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-09 11:52:12 +0000

    Board component

---
+++ src/components/Board/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './board.module.css'
+const Board = () => {
+
+    return (
+        <div className={css.board}>
+            
+        </div>
+    )
+}
+
+export default Board
---
+++ src/components/Game/index.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState } from 'react';
+import Board from '../Board';
+function Game() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default Game;
